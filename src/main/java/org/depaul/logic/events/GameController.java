369ddfcfc4e45846da8a489e1a471669b4992b50
calebpapay <calebpapay@gmail.com>
2022-11-01 15:53:51 -0500

    adding files

---
+++ src/main/java/org/depaul/logic/events/GameController.java
@@ -0,0 +1,73 @@
+package org.depaul.logic.events;
+
+import org.depaul.gui.GuiController;
+import org.depaul.logic.board.Board;
+import org.depaul.logic.board.SimpleBoard;
+import org.depaul.logic.data.ViewData;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GameController implements InputEventListener {
+
+    private final Board board = new SimpleBoard(25, 10);
+
+    private final GuiController viewGuiController;
+
+    public GameController(GuiController c) {
+        viewGuiController = c;
+        board.createNewBrick();
+        viewGuiController.setEventListener(this);
+        viewGuiController.initGameView(board.getBoardMatrix(), board.getViewData());
+        viewGuiController.bindScore(board.getScore().scoreProperty());
+    }
+
+    @Override
+    public ViewData onMoveEvent(MoveEvent moveEvent) {
+        switch (moveEvent.getEventType()) {
+            case UP:
+                //(this handles rotateBrick, so brickRotator.setCurrentShapeIndex() doesn't need to be called twice)
+                //if the brick couldn't be rotated
+                if (!board.rotateBrick()) {
+                    //nothing to do here i guess
+                }
+                break;
+            case DOWN:
+                //(this handles moveBrickDown, so p.translate() doesn't need to be called twice)
+                //if the brick couldn't move down
+                if (!board.moveBrickDown()) {
+                    board.mergeBrickToBackground();
+                    if (board.createNewBrick()) {
+                        viewGuiController.gameOver();
+                    }
+                    viewGuiController.refreshGameBackground(board.getBoardMatrix());
+                }
+                break;
+            case LEFT:
+                //(this handles moveBrickLeft, so p.translate() doesn't need to be called twice)
+                //if the brick couldn't move left
+                if (!board.moveBrickLeft()) {
+                    board.mergeBrickToBackground();
+                    if (board.createNewBrick()) {
+                        viewGuiController.gameOver();
+                    }
+                    viewGuiController.refreshGameBackground(board.getBoardMatrix());
+                }
+                break;
+            case RIGHT:
+                //(this handles moveBrickLeft, so p.translate() doesn't need to be called twice)
+                //if the brick couldn't move left
+                //TODO implement this
+                break;
+            default:
+                break;
+        }
+
+        return board.getViewData();
+    }
+
+    @Override
+    public void createNewGame() {
+        board.newGame();
+        viewGuiController.refreshGameBackground(board.getBoardMatrix());
+    }
+}
